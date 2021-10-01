aedefa50a3115676a40442dbdb6b992416a96fb9
Adris Jautakas <adrisj7@gmail.com>
2021-10-01 01:53:41 -0400

    Add simple Example Command

--- src/main/java/adris/example/commands/ExampleCommand.java
+++ src/main/java/adris/example/commands/ExampleCommand.java
@@ -4,6 +4,7 @@ import adris.altoclef.AltoClef;
 import adris.altoclef.commandsystem.ArgParser;
 import adris.altoclef.commandsystem.Command;
 import adris.altoclef.commandsystem.CommandException;
+import adris.example.tasks.PunkCowsExampleTask;
 
 public class ExampleCommand extends Command {
     public ExampleCommand() {
@@ -13,5 +14,6 @@ public class ExampleCommand extends Command {
     @Override
     protected void Call(AltoClef altoClef, ArgParser argParser) throws CommandException {
         altoClef.log("Hello World!");
+        altoClef.runUserTask(new PunkCowsExampleTask(), this::finish);
     }
 }
---
+++ src/main/java/adris/example/tasks/PunkCowsExampleTask.java
@@ -0,0 +1,48 @@
+package adris.example.tasks;
+
+import adris.altoclef.AltoClef;
+import adris.altoclef.TaskCatalogue;
+import adris.altoclef.tasks.entity.KillEntitiesTask;
+import adris.altoclef.tasksystem.Task;
+import net.minecraft.entity.passive.CowEntity;
+
+public class PunkCowsExampleTask extends Task {
+
+    @Override
+    protected void onStart(AltoClef altoClef) {
+        // Run for the first frame of this task.
+    }
+
+    @Override
+    protected Task onTick(AltoClef altoClef) {
+        // Run every frame this task runs.
+
+        /* This task will:
+         * - Get an iron sword if we don't have one
+         * - Attack cows
+         */
+        if (!altoClef.getInventoryTracker().hasItem("iron_sword")) {
+            return TaskCatalogue.getItemTask("iron_sword", 1);
+        }
+        return new KillEntitiesTask(CowEntity.class);
+    }
+
+    @Override
+    protected void onStop(AltoClef altoClef, Task task) {
+        // Run after this task stops or is interrupted.
+        // the `task` variable will hold the task that interrupted this one (can be null), but it's rarely (if ever) used
+    }
+
+    @Override
+    protected boolean isEqual(Task task) {
+        // This is VERY important when running sub tasks.
+        // Make sure EVERY task has a reliable "isEqual" function.
+        return task instanceof PunkCowsExampleTask;
+    }
+
+    @Override
+    protected String toDebugString() {
+        // Used for debug printing.
+        return "Doing Example Stuff";
+    }
+}
